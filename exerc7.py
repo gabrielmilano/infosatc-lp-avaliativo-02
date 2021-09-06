6fb26b729c65d0b35db3dad9f79c722528b5585c
gabrielmilano <79032997+gabrielmilano@users.noreply.github.com>
2021-09-06 17:07:03 -0300

    Create exerc7.py

---
+++ exerc7.py
@@ -0,0 +1,29 @@
+filmes  = ["Trocando os pés","Ultimo homem"]
+print(filmes)
+jogos   = ["Army men","Ratchet and clank"]
+print(jogos)
+livros  = ["Diario de um banana","Diário de Annie Frank"]
+print(livros)
+esporte = ["Volei","Basquete"]
+print(esporte)
+filmes.append("Thor ragnarok")
+filmes.insert(-3, "Homem aranha de volta ao lar")
+print(filmes)
+jogos.append("Crash")
+jogos.insert(-2, "The last of us")
+print(jogos)
+livros.append("1984")
+livros.insert(0, "Frankstain")
+print(livros)
+esporte.append("Volei")
+esporte.insert(-3, "Baseball")
+print(esporte)
+lista = [filmes,jogos,livros,esporte]
+print(lista)
+print(livros[0])
+esporte.remove("Volei")
+print(esporte)
+disciplina = ["Historia","Matemática"]
+print(disciplina)
+lista.append(disciplina)
+print(lista)
