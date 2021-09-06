c437762d85037d9715f01c50eca6ab96855dbf55
gabrielmilano <79032997+gabrielmilano@users.noreply.github.com>
2021-09-06 17:07:06 -0300

    Create exerc8.py

---
+++ exerc8.py
@@ -0,0 +1,5 @@
+pc_gamer = ["Processador i9 10900k","64Gb RAM DDR6","Gabinete cooler master","Placa de Vídeo a geforce RTX 2080Ti"]
+print(pc_gamer)
+pc_gamer.remove("64Gb RAM DDR6")
+pc_gamer.remove("Processador i9 10900k")
+print(pc_gamer)
