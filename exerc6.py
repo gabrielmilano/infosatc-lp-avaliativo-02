7fda41a34733edf47b5119ac90345677eb093d07
gabrielmilano <79032997+gabrielmilano@users.noreply.github.com>
2021-09-06 17:06:56 -0300

    Create exerc6.py

---
+++ exerc6.py
@@ -0,0 +1,3 @@
+pesquisa = input("Digite um item da lista para descobrir onde esta :")
+lista = ['BANANA', 'MAÇA', 'LARANJA', 'MORANGO','UVA','MELAO']
+print("Encontrando a posição de um item na lista: ", lista.index(pesquisa))
